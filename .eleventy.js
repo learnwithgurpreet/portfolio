83cd20b9b594aea84e5a9c80aa4c23a2195a3066
Gurpreet Singh <gursingh7@publicisgroupe.net>
2022-07-11 10:16:22 +0200

    added cache buster

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,19 @@
+const cacheBuster = require("@mightyplow/eleventy-plugin-cache-buster");
 const { JSDOM } = require("jsdom");
+const package = require("./package.json")
+
+const cacheBusterOptions = {
+  createResourceHash(outputDirectoy, url, target) {
+    const { version } = package;
+    return version;
+  },
+};
 
 module.exports = function (eleventyConfig) {
-  // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("assets");
 
   eleventyConfig.setTemplateFormats("njk");
+  eleventyConfig.addPlugin(cacheBuster(cacheBusterOptions));
 
   eleventyConfig.addTransform("lazy-load-images", (content, outputPath) => {
     if (outputPath.endsWith(".html")) {
